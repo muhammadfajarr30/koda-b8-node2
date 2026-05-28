9fd080e618988bda64da28858c5fa100bacb2ae9
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-05-28 20:13:52 +0700

    feat: add gitignore for node_modules

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-const moment = require("moment");
 const readline = require("node:readline");
+const convertTanggal = require("../functions/convertTanggal.js");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
-function convertTanggal(input) {
-  const cekTanggal = moment(input, "DD-MM-YYYY", true).isValid();
-
-  if (!cekTanggal) {
-    console.log("format tanggal salah");
-    return;
-  }
-
-  const ubahFormat = moment(input, "DD-MM-YYYY").format("DD/MM/YYYY");
-  return ubahFormat;
-}
 
 rl.question("Input date: ", (ans) => {
   const result = convertTanggal(ans);
